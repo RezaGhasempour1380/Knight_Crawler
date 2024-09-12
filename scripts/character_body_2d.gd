131bcd3d99243a6d24301f155720566065d50a84
RezaGhasempour1380 <rezaghasempour1380@gmail.com>
2024-09-12 13:31:41 +0300

    new texture and animation

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -9,24 +9,41 @@ extends CharacterBody2D
 @onready var cshape = $CollisionShape2D
 @onready var crouch_raycast_1 = $crouch_raycast_1
 @onready var crouch_raycast_2 = $crouch_raycast_2
+@onready var coyote_timer = $CoyoteTimer
+@onready var attack_hitbox = $attack_hitbox
 
 var is_crouching = false
 var stuck_under_object = false
+var attack_mode = false
+var can_coyote_jump = false
+var attack_toggle_normal_no_move = true
+var attack_toggle_crouch = true  
 
 var standing_cshape = preload("res://resources/player_standing_collision_shape.tres")
 var crouching_cshape = preload("res://resources/player_crouching_collision_shape.tres")
+var atk_standing_csshape = preload("res://resources/player_standing_attack_collision.tres")
+var atk_crouch_csshape = preload("res://resources/player_crouching_attack_collision.tres")
+
+var saved_velocity = Vector2.ZERO
 
 func _physics_process(_delta):
-	if !is_on_floor():
+	if !is_on_floor() && (can_coyote_jump == false):
 		velocity.y += gravity
 		if velocity.y > 1000:
 			velocity.y = 1000
 	
-	if Input.is_action_just_pressed("jump") && is_on_floor():
-		velocity.y = -jump_force
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() || can_coyote_jump:
+			velocity.y = -jump_force
+			if can_coyote_jump:
+				can_coyote_jump = false;
 	
 	var horizontal_direction = Input.get_axis("move_left","move_right")
-	velocity.x = speed * horizontal_direction
+	
+	if !attack_mode:
+		velocity.x = speed * horizontal_direction
+	else:
+		velocity.x = 0
 	
 	if horizontal_direction != 0:
 		switch_direction(horizontal_direction)
@@ -45,20 +62,42 @@ func _physics_process(_delta):
 			stand()
 			stuck_under_object = false
 	
+	if Input.is_action_just_pressed("attack") and !attack_mode and is_on_floor():
+			attack_mode = true
+			saved_velocity = velocity 
+			velocity.x = 0  
+			attack()
+	
+	if attack_mode and !ap.is_playing():
+		attack_mode = false
+		velocity = saved_velocity
+	
+	var was_on_floor = is_on_floor(); 
+
 	move_and_slide()
 	
-	update_animations(horizontal_direction)
+	if was_on_floor && !is_on_floor() && velocity.y >= 0:
+		can_coyote_jump = true
+		coyote_timer.start()
 	
+	update_animations(horizontal_direction)
+
+func _on_coyote_timer_timeout():
+	can_coyote_jump = false
+
 func above_head_empty() -> bool:
 	var result = !crouch_raycast_1.is_colliding() && !crouch_raycast_2.is_colliding() 
 	return result
 	
 func update_animations(horizontal_direction):
+	if attack_mode:
+		return
+	
 	if is_on_floor():
 		if horizontal_direction == 0:
 			if is_crouching:
 				ap.play("crouch")
-			else:
+			elif attack_mode == false:
 				ap.play("idle")
 		else:
 			if is_crouching:
@@ -73,7 +112,7 @@ func update_animations(horizontal_direction):
 	
 func switch_direction(horizontal_direction):
 	sprite.flip_h = (horizontal_direction == -1)
-	sprite.position.x = horizontal_direction * 1
+	sprite.position.x = horizontal_direction * 5
 	
 func crouch():
 	if is_crouching:
@@ -81,7 +120,7 @@ func crouch():
 	is_crouching = true
 	speed = 50
 	cshape.shape = crouching_cshape
-	cshape.position.y = -3.5
+	cshape.position.y = -13.75
 	
 func stand():
 	if is_crouching == false:
@@ -89,5 +128,18 @@ func stand():
 	is_crouching = false
 	speed = 100
 	cshape.shape = standing_cshape
-	cshape.position.y = -5.5
+	cshape.position.y = -19
 	
+func attack():
+	if is_crouching:
+		if attack_toggle_crouch:
+			ap.play("crouch_atk") 
+	else:
+		if attack_toggle_normal_no_move:
+			ap.play("atk_no_move_1")
+		else:
+			ap.play("atk_no_move_2")
+		attack_toggle_normal_no_move = !attack_toggle_normal_no_move
+
+
+
